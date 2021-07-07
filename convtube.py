71fa595e10f205a8269e3cf0e41870663b2a4cf4
alpkunt777 <83122328+alpkunt777@users.noreply.github.com>
2021-07-07 17:14:25 +0300

    Update convtube.py

--- convtube.py
+++ convtube.py
@@ -7,11 +7,11 @@ from colorama import init,Fore,Back,Style
 os.system("clear")
 init()
 
-options = input(f"{Fore.YELLOW}DownTube is a Youtube video forwarding program.\nMy Discord: Alpkunt#4620\n\n{Fore.GREEN}[1] {Fore.CYAN}Convert mp3\n{Fore.GREEN}[2] {Fore.CYAN}Convert mp4\n{Fore.GREEN}\n{Fore.LIGHTRED_EX}downtube >>> {Style.RESET_ALL}")
+options = input(f"{Fore.YELLOW}ConvTube is a Youtube video convert program.\nMy Discord: Alpkunt#4620\n\n{Fore.GREEN}[1] {Fore.CYAN}Convert mp3\n{Fore.GREEN}[2] {Fore.CYAN}Convert mp4\n{Fore.GREEN}\n{Fore.LIGHTRED_EX}downtube >>> {Style.RESET_ALL}")
 
 
 if options == "1":
-	url = input(f"\n{Fore.YELLOW}Please paste the url of the youtube video you want to convert.\n\n{Fore.LIGHTRED_EX}downtube >>> {Style.RESET_ALL}")
+	url = input(f"\n{Fore.YELLOW}Please paste the url of the youtube video you want to convert.\n\n{Fore.LIGHTRED_EX}convtube >>> {Style.RESET_ALL}")
 
 	os.system("clear")
 
@@ -26,11 +26,11 @@ if options == "1":
 
 	youtube_dl.YoutubeDL(ydl_opts).download([url])
     	
-	print(f"{Fore.YELLOW}\n\nConvert is complated succesfully! Pleasee check DownTube directory.")
+	print(f"{Fore.YELLOW}\n\nConvert is complated succesfully! Pleasee check ConvTube directory.")
 
 
 if options == "2":
-	url = input(f"\n{Fore.YELLOW}Please paste the url of the youtube video you want to convert.\n\n{Fore.LIGHTRED_EX}downtube >>> {Style.RESET_ALL}")
+	url = input(f"\n{Fore.YELLOW}Please paste the url of the youtube video you want to convert.\n\n{Fore.LIGHTRED_EX}convtube >>> {Style.RESET_ALL}")
 
 	YouTube(url).streams.first().download()
-	print(f"{Fore.YELLOW}\n\nConvert is complated succesfully! Pleasee check DownTube directory.")
+	print(f"{Fore.YELLOW}\n\nConvert is complated succesfully! Pleasee check ConvTube directory.")
